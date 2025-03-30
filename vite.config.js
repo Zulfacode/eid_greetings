d237c0809a358140eb2a28469d91c1203e342b54
Hayfa Bougrine <zulfacoder@gmail.com>
2025-03-30 13:18:15 +0200

    fix vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -4,20 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-        assetFileNames: 'assets/[name].[hash][extname]',
-        chunkFileNames: 'assets/[name].[hash].js',
-        entryFileNames: 'assets/[name].[hash].js'
-      }
-    },
-    target: 'es2015',
-    modulePreload: false
-  },
+  build: "/eid_greetings",
   server: {
     port: 3000,
     proxy: {
